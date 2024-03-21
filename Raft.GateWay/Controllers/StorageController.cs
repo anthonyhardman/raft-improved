7a99797da2136b8ac4c9a6ac34fa061328ee738f
anthony <anthonythardman@gmail.com>
2024-03-21 16:26:53 -0600

    gateway fixes

--- Raft.GateWay/Controllers/StorageController.cs
+++ Raft.GateWay/Controllers/StorageController.cs
@@ -5,36 +5,42 @@ using Raft.Shared.Models;
 
 namespace MyApp.Namespace
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StorageController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class StorageController : ControllerBase
+  {
+    private readonly List<IRaftNode> _raftNodes;
+
+    public StorageController(List<IRaftNode> raftNodes)
     {
-        private readonly IRaftNode _raftNode;
-
-        public StorageController(IRaftNode raftNode)
-        {
-            _raftNode = raftNode;
-        }
-
-        [HttpGet("strong")]
-        public async Task<ActionResult<string>> StrongGet(string key)
-        {
-            var response = await _raftNode.StrongGet(key);
-            return Ok(response);
-        }
-
-        [HttpGet("eventual")]
-        public async Task<ActionResult<string>> EventualGet(string key)
-        {
-            var response = await _raftNode.EventualGet(key);
-            return Ok(response);
-        }
-
-        [HttpPost("compare-and-swap")]
-        public async Task<ActionResult<bool>> CompareAndSwap(CompareAndSwapRequest request)
-        {
-            var response = await _raftNode.CompareAndSwap(request);
-            return Ok(response);
-        }
+      _raftNodes = raftNodes;
     }
+
+    [HttpGet("strong")]
+    public async Task<ActionResult<string>> StrongGet(string key)
+    {
+      var node = _raftNodes.First();
+
+      var response = await node.StrongGet(key);
+      return Ok(response);
+    }
+
+    [HttpGet("eventual")]
+    public async Task<ActionResult<string>> EventualGet(string key)
+    {
+      var node = _raftNodes.First();
+
+      var response = await node.EventualGet(key);
+      return Ok(response);
+    }
+
+    [HttpPost("compare-and-swap")]
+    public async Task<ActionResult<bool>> CompareAndSwap(CompareAndSwapRequest request)
+    {
+      var node = _raftNodes.First();
+
+      var response = await node.CompareAndSwap(request);
+      return Ok(response);
+    }
+  }
 }
--- Raft.GateWay/Program.cs
+++ Raft.GateWay/Program.cs
@@ -6,12 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var node_ids = (Environment.GetEnvironmentVariable("NODE_IDS") ?? "").Split(",");
+var node_addresses = (Environment.GetEnvironmentVariable("NODE_ADDRESSES") ?? "").Split(",");
+var nodes = node_ids.Zip(node_addresses, (id, address) =>
+{
+    var node = new GrpcRaftNode(id, address);
+    return node;
+}).ToList<IRaftNode>();
+
+builder.Services.AddSingleton<List<IRaftNode>>(nodes);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<IRaftNode>(sp => {
-    return new GrpcRaftNode("http://localhost:5281");
-});
 
 var app = builder.Build();
 
